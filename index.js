35cbcabee3eb7957593fc38ef783a4e33ef0f9fe
impulse <ecklf@icloud.com>
2020-07-06 18:58:32 +0200

    fix: fix path for cwd

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const fs = require("fs");
 const resolveConfig = require("tailwindcss/resolveConfig");
-const tailwindConfig = require("./tailwind.config.js");
+const tailwindConfig = require(process.cwd() + "/tailwind.config.js");
 
 try {
   let fullConfig = resolveConfig(tailwindConfig);
   fullConfig = JSON.stringify(fullConfig, null, 2);
 
-  fs.writeFileSync("./tailwind.json", fullConfig, {
+  fs.writeFileSync(process.cwd() + "/tailwind.json", fullConfig, {
     encoding: "utf-8",
   });
 } catch (error) {
