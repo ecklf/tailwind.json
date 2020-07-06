f0711419ad088f76d754d6568bd1086555308d2d
impulse <ecklf@icloud.com>
2020-07-06 18:51:16 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const resolveConfig = require("tailwindcss/resolveConfig");
+const tailwindConfig = require(__dirname + "/tailwind.config.js");
+
+try {
+  let fullConfig = resolveConfig(tailwindConfig);
+  fullConfig = JSON.stringify(fullConfig, null, 2);
+
+  fs.writeFileSync(__dirname + "/tailwind.json", fullConfig, {
+    encoding: "utf-8",
+  });
+} catch (error) {
+  console.error(error);
+}
