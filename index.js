cb5fa3ccd63688a4402c4f53cbbdf52fddf5a8df
impulse <ecklf@icloud.com>
2020-07-06 18:57:12 +0200

    fix: fix path

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 const fs = require("fs");
 const resolveConfig = require("tailwindcss/resolveConfig");
-const tailwindConfig = require(__dirname + "/tailwind.config.js");
+const tailwindConfig = require("./tailwind.config.js");
 
 try {
   let fullConfig = resolveConfig(tailwindConfig);
   fullConfig = JSON.stringify(fullConfig, null, 2);
 
-  fs.writeFileSync(__dirname + "/tailwind.json", fullConfig, {
+  fs.writeFileSync("./tailwind.json", fullConfig, {
     encoding: "utf-8",
   });
 } catch (error) {
